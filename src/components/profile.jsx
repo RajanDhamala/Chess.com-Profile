2fe10d63c480adc1db47dd76e673596c38a85e67
rajan <rajandhamala0123@gmail.com>
2024-08-03 21:22:55 +0545

    update

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -9,16 +9,15 @@ async function Country(){
         data3=await data5.json()
         Setcountry(data3.name)
         console.log(data3.name)
-
     }catch(error){
         console.log('Error',error)
     }
+    return data3
 }
+const thulomah='text-2xl'
 
 useEffect(()=>{
-    if (!country){
         Country()
-    }
    
 },[result.name])
     
@@ -35,17 +34,16 @@ useEffect(()=>{
     }
     return(
         <>
-    <div className="">
-    <div className="flex flex-col items-center mx-auto container py-2">
+    <div className="flex flex-col items-center mx-auto container py-2 bg-gray-300 ">
     <img src={result.avatar || 'https://static.thenounproject.com/png/55168-200.png'} className="w-20" />
-    <h1 className="text-lg font-bold mt-2 text-center">{result.title} {result.name}</h1>
-    <p className="text-wrap text-center text-lg font-semibold">Location:{country} {result.location}</p>
-    <p className="text-1xl font-semibold">Followers:{ formatNumber(result.followers)}</p>
-    <p className="text-1xl font-semibold">Last Online:{getDate(result.last_online)}</p>
-    <p className="text-1xl font-semibold">Player id:{result.player_id}</p>
+    <h1 className="text-lg font-bold mt-2 text-center md:text-2xl md:text-center">{result.title} {result.name}</h1>
+    <p className="text-wrap text-center text-lg font-semibold md:text-2xl md:text-center">Location:{country} {result.location}</p>
+    <p className="text-1xl font-semibold md:text-2xl md:text-center">Followers:{ formatNumber(result.followers)}</p>
+    <p className="text-1xl font-semibold md:text-2xl md:text-center">Last Online:{getDate(result.last_online)}</p>
+    <p className="text-1xl font-semibold md:text-2xl md:text-center">Player id:{result.player_id}</p>
     <a href={result.url} target="_blank" className="underline text-blue-600 font-semibold">Player Profile</a>
 
-    </div></div>
+    </div>
     </>
 
 )}
