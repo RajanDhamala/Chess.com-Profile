949f5d650761a77595c12c41e60c4f80425a7fcf
rajan <rajandhamala0123@gmail.com>
2024-08-08 21:38:19 +0545

    done re using components fixed some issues and fetched game logs as filtered by date and reflected in the ui

--- src/components/GameUI.jsx
+++ src/components/GameUI.jsx
@@ -42,15 +42,18 @@ export default function(){
     const handlefetch=(e)=>{
         e.preventDefault()
         console.log(date)
+        if (input==''){
+          alert("please enter username")
+        }
         async function getresp(){
         try{
-        const resp= await fetch(`https://api.chess.com/pub/player/NbcWala/games/${date}`)
+        const resp= await fetch(`https://api.chess.com/pub/player/${input}/games/${date}`)
         const data=await resp.json()
         console.log(data)
         if (data.games.length==0){
           alert("Games not found")
         }
-        
+
         Setapibataako(data)
         console.log(data.games[0].time_class)
         }catch(error){
@@ -64,11 +67,33 @@ export default function(){
       if (apibataako && apibataako.games && apibataako.games.length > 0) {
           console.log("Updated Time Class in Effect: ", apibataako.games[0].time_class);
       }
-  }, [apibataako]);
+  }, [apibataako,]);
+
+  const [input,Setinput]=useState('')
+  
+  function handleinput(e){
+    Setinput(e.target.value)
+  }
+  function handlesearch(e){
+    e.preventDefault()
+  }
     
     return(
         <>
-        
+        <section className="my-5">
+        <form class="max-w-md mx-auto">   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="hikaru/NbcWala" required value={input} onChange={(e)=>{handleinput(e)}} />
+        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={(e)=>{handlesearch(e)}}>Search</button>
+    </div>
+</form>
+        </section>
         <div className="bg-gray-500">
             <h1 className="text-white text-center text-2xl font-bold">Select the game</h1>
             <h1 className="text-white text-center text-2xl font-bold">{date}</h1>
@@ -76,7 +101,7 @@ export default function(){
             <div className="overflow-y-scroll md:max-h-96 max-h-60">
             {apibataako && apibataako.games && apibataako.games.length > 0 && 
               apibataako.games.map((game,index)=>(
-                <Gamelog key={index} gamedata={game}/>
+                <Gamelog gamedata={game}/>
               ))
             }
           </div>
